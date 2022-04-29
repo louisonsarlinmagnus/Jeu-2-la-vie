bebac129c23921eb9416f0ebbda4e84a118c4972
Louison SARLIN--MAGNUS <lsarlinmagnus@gmail.com>
2022-04-29 15:41:25 +0000

    Fix grille evolution

--- app/jeuDeLaVie/views.py
+++ app/jeuDeLaVie/views.py
@@ -3,16 +3,19 @@ from django.http import HttpResponse
 from jeuDeLaVie.classes.grille import Grille
 import time
 
+vie = Grille(25, 10)
+vie.remplir_alea(25)
+vie.affecte_voisins()
 
 def index(request):
-    vie = Grille(25, 10)
-    vie.remplir_alea(55)
-    vie.affecte_voisins()
+    # vie = Grille(25, 10)
+    # vie.remplir_alea(5)
+    # vie.affecte_voisins()
     # while True:
     #     print("\u001B[H\u001B[J") #On met un caractère d'échapement
     #     print(vie) #On affiche la grille
     #     print("\n") #On saute une ligne pour bien voir la différence avec la pahse d'avant
     #     time.sleep(0.5)
-    #     vie.jeu()
-    #     vie.actualise()
+    vie.jeu()
+    vie.actualise()
     return HttpResponse(vie)
