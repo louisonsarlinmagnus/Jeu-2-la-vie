286e30666b7ea6a643ea51231434e6b124676570
Louison SARLIN--MAGNUS <lsarlinmagnus@gmail.com>
2022-04-30 15:46:15 +0000

    Ajout du concept de forme

--- app/jeuDeLaVie/models.py
+++ app/jeuDeLaVie/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Forme(models.Model):
+    nom = models.CharField('Nom de la forme', max_length=20) #Bloc
+    description = models.CharField('Description de la forme', max_length=200) #L'objet le plus commun du jeu de la vie. C'est la plus petite structure stable.
+    cellules = models.TextField('Cellules qui composent la forme') #[[0,0],[0,1],[1,0],[1,1]]
+
+    def __str__(self) -> str:
+        return (str(self.nom,": ",self.desccription))
